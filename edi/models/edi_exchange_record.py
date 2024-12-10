5d9e292a8f7ccf3b8a66c89bbf2c73a67da3614d
Bastian Guenther <Bastian.Guenther@ametras.com>
2024-12-10 14:03:02 +0100

    [FIX] edi: Fixed indent

--- edi/models/edi_exchange_record.py
+++ edi/models/edi_exchange_record.py
@@ -214,7 +214,7 @@ class EDIExchangeRecord(models.Model):
         return super().create(vals)
 
     def _get_identifier(self):
-            return self.env["ir.sequence"].next_by_code("edi.exchange") or "New"
+        return self.env["ir.sequence"].next_by_code("edi.exchange") or "New"
 
     @api.constrains("backend_id", "type_id")
     def _constrain_backend(self):
